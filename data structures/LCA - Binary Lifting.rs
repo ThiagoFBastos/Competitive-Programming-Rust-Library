e29f7c0b421b805c4bc42532433b8f4da9d2b756
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-12-31 14:56:01 -0300

    refactor: swapping the rows and columns of sparse table to improve cache access

--- data structures/LCA - Binary Lifting.rs
+++ data structures/LCA - Binary Lifting.rs
@@ -4,128 +4,134 @@
  * Description: Data structure that can answer the lca between two vertexes and the distance of them.
  */
 
-struct LCA {
-   adj: Vec<Vec<(usize, i64)>>, // the adjacency list with weights
-   depth: Vec<i32>, // the depth/height of each vertex
-   st: Vec<Vec<usize>>, // the sparse table for jumps
-   weight: Vec<i64> // the sum of weights from the root to each vertex
-}
+mod data_structures {
+   
+   pub mod graphs {
 
-impl LCA {
-
-    /**
-     * Create a new instance of LCA data structure
-     * @param n the number of nodes
-     * @return the new LCA data structure
-     */
-   fn new(n: usize) -> Self {
-
-      assert_ne!(n, 0);
-
-      let lg = 32 - (n as i32).leading_zeros();
-
-      Self {
-         adj: vec![vec![]; n],
-         depth: vec![0; n],
-         st: vec![vec![0; n]; lg as usize],
-         weight: vec![0; n]
-      }
-   }
-
-   /**
-    * Do a DFS from the root to the vertexes to calculate some properties
-    * @param u the vertex to do the search
-    * @param p the parent of vertex u
-    */
-   fn dfs(&mut self, u: usize, p: usize) {
-      self.st[0][u] = p;
-
-      for i in 1..self.st.len() {
-         self.st[i][u] = self.st[i - 1][self.st[i - 1][u]];
+      #[derive(Clone)]
+      pub struct LCA {
+         adj: Vec<Vec<(usize, i64)>>, // the adjacency list with weights
+         depth: Vec<i32>, // the depth/height of each vertex
+         st: Vec<Vec<usize>>, // the sparse table for jumps
+         weight: Vec<i64> // the sum of weights from the root to each vertex
       }
 
-      for &(v, w) in self.adj[u].clone().iter() {
-         if v == p {
-            continue;
+      impl LCA {
+
+         /**
+          * Create a new instance of LCA data structure
+          * @param n the number of nodes
+          * @return the new LCA data structure
+          */
+         pub fn new(n: usize) -> Self {
+
+            assert_ne!(n, 0);
+
+            let lg = 32 - (n as i32).leading_zeros();
+
+            Self {
+               adj: vec![vec![]; n],
+               depth: vec![0; n],
+               st: vec![vec![0; lg as usize]; n],
+               weight: vec![0; n]
+            }
          }
 
-         self.depth[v] = 1 + self.depth[u];
-         self.weight[v] = w + self.weight[u];
-         self.dfs(v, u);         
-      }
-   }
+         /**
+          * Do a DFS from the root to the vertexes to calculate some properties
+          * @param u the vertex to do the search
+          * @param p the parent of vertex u
+          */
+         fn dfs(&mut self, u: usize, p: usize) {
+            self.st[u][0] = p;
 
-   /**
-    * Build the LCA data structure using a vertex as root
-    * @param root the root of the tree
-    */
-   fn build(&mut self, root: usize) {
-      self.depth[root] = 0;
-      self.weight[root] = 0;
-      self.dfs(root, root);
-   }
+            for i in 1..self.st[u].len() {
+               self.st[u][i] = self.st[self.st[u][i - 1]][i - 1];
+            }
 
-   /**
-    * Add an undirected edge between u and v with weight w, being that w = 1 if the tree is unweighted
-    * @param u a vertex of the edge
-    * @param v a vertex of the edge
-    * @param w the weight of the edge
-    */
-   fn add_edge(&mut self, u: usize, v: usize, w: i64) {
-      self.adj[u].push((v, w));
-      self.adj[v].push((u, w));
-   }
+            for &(v, w) in self.adj[u].clone().iter() {
+               if v == p {
+                  continue;
+               }
 
-   /**
-    * Find the LCA of two vertexes
-    * @param u a vertex of pair to find the lca
-    * @param v a vertex of pair to find the lca
-    * @return the lca of u and v
-    */
-   fn query(&self, mut u: usize, mut v: usize) -> usize {
-      if self.depth[u] > self.depth[v] {
-         std::mem::swap(&mut u, &mut v);
-      }
-
-      let height = self.depth[v] - self.depth[u];
-
-      if height != 0 {
-         let lg = 31 - height.leading_zeros() as usize;
-         
-         for i in 0..=lg {
-            if (height >> i) & 1 == 1 {
-               v = self.st[i][v];
-            } 
-         }
-      }
-
-      if u == v {
-         return u;
-      }
-
-      let lg = 31 - self.depth[u].leading_zeros() as usize;
-
-      for i in (0..=lg).rev() {
-         if self.st[i][u] == self.st[i][v] {
-            continue;
+               self.depth[v] = 1 + self.depth[u];
+               self.weight[v] = w + self.weight[u];
+               self.dfs(v, u);         
+            }
          }
 
-         u = self.st[i][u];
-         v = self.st[i][v];
+         /**
+          * Build the LCA data structure using a vertex as root
+          * @param root the root of the tree
+          */
+         pub fn build(&mut self, root: usize) {
+            self.depth[root] = 0;
+            self.weight[root] = 0;
+            self.dfs(root, root);
+         }
+
+         /**
+          * Add an undirected edge between u and v with weight w, being that w = 1 if the tree is unweighted
+          * @param u a vertex of the edge
+          * @param v a vertex of the edge
+          * @param w the weight of the edge
+          */
+         pub fn add_edge(&mut self, u: usize, v: usize, w: i64) {
+            self.adj[u].push((v, w));
+            self.adj[v].push((u, w));
+         }
+
+         /**
+          * Find the LCA of two vertexes
+          * @param u a vertex of pair to find the lca
+          * @param v a vertex of pair to find the lca
+          * @return the lca of u and v
+          */
+         pub fn query(&self, mut u: usize, mut v: usize) -> usize {
+            if self.depth[u] > self.depth[v] {
+               std::mem::swap(&mut u, &mut v);
+            }
+
+            let height = self.depth[v] - self.depth[u];
+
+            if height != 0 {
+               let lg = 31 - height.leading_zeros() as usize;
+               
+               for i in 0..=lg {
+                  if (height >> i) & 1 == 1 {
+                     v = self.st[v][i];
+                  } 
+               }
+            }
+
+            if u == v {
+               return u;
+            }
+
+            let lg = 31 - self.depth[u].leading_zeros() as usize;
+
+            for i in (0..=lg).rev() {
+               if self.st[u][i] == self.st[v][i] {
+                  continue;
+               }
+
+               u = self.st[u][i];
+               v = self.st[v][i];
+            }
+
+            return self.st[u][0];
+         }
+
+         /**
+          * Calculate the distance between two nodes
+          * @param u the vertex of the pair to find the distance
+          * @param v the vertex of the pair to find the distance
+          * @return the distance between u and v
+          */
+         pub fn distance(&self, u: usize, v: usize) -> i64 {
+            let x = self.query(u, v);
+            return self.weight[u] + self.weight[v] - 2 * self.weight[x];
+         }
       }
-
-      return self.st[0][u];
    }
-
-   /**
-    * Calculate the distance between two nodes
-    * @param u the vertex of the pair to find the distance
-    * @param v the vertex of the pair to find the distance
-    * @return the distance between u and v
-    */
-   fn distance(&self, u: usize, v: usize) -> i64 {
-      let x = self.query(u, v);
-      return self.weight[u] + self.weight[v] - 2 * self.weight[x];
-   }
-
 }
