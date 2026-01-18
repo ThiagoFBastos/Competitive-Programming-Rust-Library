9ac8993d8b2a2904a27add46c808beda3c5c7964
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-01-18 19:21:06 -0300

    refactor: making the Fenwick Tree agnostic to operations

--- data structures/Fenwick Tree.rs
+++ data structures/Fenwick Tree.rs
@@ -1,58 +1,66 @@
 /*
  * Author: Thiago Felipe Bastos da Silva
  * Created: 2025-12-28
- * Description: Simple Fenwick Tree data structure.
+ * Description: Simple Fenwick Tree data structure agnostic to operations.
  */
 
-#[derive(Clone)]
-struct FenwickTree<T> {
-   sum: Vec<T>,
-   length: usize
+trait Constants {
+
+   fn initial() -> Self; // the initial constant
 }
 
-impl<T: Default + Copy + AddAssign> FenwickTree<T> {
+#[derive(Clone)]
+struct FenwickTree<T, OP> {
+   data: Vec<T>, // the fenwick tree data
+   length: usize, // the number of elements of the fenwick tree
+   op: OP // the binary operator to apply an operation in the fenwick tree
+}
+
+impl<T: Constants + Copy, OP: Fn(T, T) -> T> FenwickTree<T, OP> {
 
     /**
      * create a new instance of FenwickTree
-     * @param length the number of elements of Fenwick Tree
-     * @return the new instance of FenwickTree with zero values 
+     * @param length the number of elements of the Fenwick Tree
+     * @param op the binary function that handles with operations
+     * @return the new instance of FenwickTree
      */
-   fn new(length: usize) -> Self {
+   fn new(length: usize, op: OP) -> Self {
 
       Self {
-         sum: vec![T::default(); length + 1],
-         length
+         data: vec![T::initial(); length + 1],
+         length,
+         op
       }
    }
 
    /**
-    * find the sum of first k elements
-    * @param k the number of the first elements for which we want to calculate the sum
+    * find the result of an operation of first k elements
+    * @param k the number of the first elements for which we want to find the result
     */
    fn query(&self, mut k: i32) -> T {
-      let mut sum = T::default();
+      let mut result = T::initial();
 
       assert!(k <= self.length as i32);
 
       while k > 0 {
-         sum += self.sum[k as usize];
+         result = (self.op)(result, self.data[k as usize]);
          k -= k & -k;
       }
 
-      sum
+      result
    }
 
    /**
-    * add a value to the element at position k 
+    * update a value to the element at position k 
     * @param k the position for which we want to modify
-    * @param value the value for which we want to sum
+    * @param value the value for which we want to apply
     */
    fn update(&mut self, mut k: i32, value: T) {
 
       assert!(k > 0);
 
       while k <= self.length as i32 {
-         self.sum[k as usize] += value;
+         self.data[k as usize] = (self.op)(self.data[k as usize], value);
          k += k & -k;
       }
    }
